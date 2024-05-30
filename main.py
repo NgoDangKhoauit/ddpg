020c7b402e3571be971a52c350f57284574df5a5
Ngo Dang Khoa <131435439+NgoDangKhoauit@users.noreply.github.com>
2024-05-30 12:47:22 +0700

    fix to make sure the game stop after n iters

--- main.py
+++ main.py
@@ -4,29 +4,27 @@ import numpy as np
 from utils import plotLearning
 import os
 
-def play_multiple_times(agent, num_episodes, game, save_freq=25, random_seed=42, type='train'):
+def play_multiple_times(agent, env, num_episodes, game, save_freq=25, random_seed=42, type='train'):
     np.random.seed(random_seed)
     score_history = []
     
     if type.lower() == 'train':
-        env = gym.make(game, render_mode=None)
         game_dir = os.path.join('models', game)
         filename = game + '-alpha000025-beta00025-400-300.png'
-    else:
-        env = gym.make(game, render_mode='human')
     
     if type.lower() != 'train':
         agent.load_models()
         
     for i in range(num_episodes):
         obs, _ = env.reset()
-        done = False
+        terminated = False
+        truncated = False
         score = 0
-        while not done:
+        while not terminated and not truncated:
             act = agent.choose_action(obs)
-            new_state, reward, done, info, _ = env.step(act)
+            new_state, reward, terminated, truncated, _ = env.step(act)
             if type.lower() == 'train':
-                agent.remember(obs, act, reward, new_state, int(done))
+                agent.remember(obs, act, reward, new_state, int(terminated))
                 agent.learn()
             score += reward
             obs = new_state
@@ -49,14 +47,14 @@ def play_multiple_times(agent, num_episodes, game, save_freq=25, random_seed=42,
     return score_history
 
 if __name__ == '__main__':
-    game = 'LunarLanderContinuous-v2'
+    game = 'LunarLander-v2'
     type = 'train'
-    env = gym.make(game)
+    env = gym.make(game, render_mode='human', continuous=True)
     agent = Agent(alpha=0.000025, beta=0.00025, input_dims=[8], tau=0.001, env=env,
-                  batch_size=64,  layer1_size=400, layer2_size=300, n_actions=2, 
+                  batch_size=64,  layer1_size=400, layer2_size=300, n_actions=4, 
                   game=game)
 
-    score_history = play_multiple_times(agent, 2000, game, type=type)
+    score_history = play_multiple_times(agent, env, 100, game, save_freq=25, type=type)
     if type.lower() == 'train':
         game_dir = os.path.join('models', game)
         filename = game + '-alpha000025-beta00025-400-300.png'
