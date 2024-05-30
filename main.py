a32708cd3e43821a0f6d06f41553db89c742fdd1
Ngo Dang Khoa <131435439+NgoDangKhoauit@users.noreply.github.com>
2024-05-30 09:26:56 +0700

    saving score when saving models

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def play_multiple_times(agent, num_episodes, game, save_freq=25, random_seed=42,
     
     if type.lower() == 'train':
         env = gym.make(game, render_mode=None)
+        game_dir = os.path.join('models', game)
+        filename = game + '-alpha000025-beta00025-400-300.png'
     else:
         env = gym.make(game, render_mode='human')
     
@@ -33,6 +35,10 @@ def play_multiple_times(agent, num_episodes, game, save_freq=25, random_seed=42,
         
         if type.lower() == 'train' and i % save_freq == 0:
             agent.save_models()
+
+            with open(os.path.join(game_dir, 'scores.txt'), 'w') as f:
+                for score in score_history:
+                    f.write(str(score) + '\n')
         
         if type.lower() == 'train':
             print('episode ', i, 'score %.2f' % score,
@@ -43,19 +49,15 @@ def play_multiple_times(agent, num_episodes, game, save_freq=25, random_seed=42,
     return score_history
 
 if __name__ == '__main__':
-    game = 'Ant-v4'
+    game = 'LunarLanderContinuous-v2'
     type = 'train'
     env = gym.make(game)
-    agent = Agent(alpha=0.000025, beta=0.00025, input_dims=[27], tau=0.001, env=env,
-                  batch_size=64,  layer1_size=400, layer2_size=300, n_actions=8, 
+    agent = Agent(alpha=0.000025, beta=0.00025, input_dims=[8], tau=0.001, env=env,
+                  batch_size=64,  layer1_size=400, layer2_size=300, n_actions=2, 
                   game=game)
 
-    score_history = play_multiple_times(agent, 1000, game, type=type)
-    
-    if type.lower == 'train':
+    score_history = play_multiple_times(agent, 2000, game, type=type)
+    if type.lower() == 'train':
         game_dir = os.path.join('models', game)
         filename = game + '-alpha000025-beta00025-400-300.png'
         plotLearning(score_history, os.path.join(game_dir, filename), window=100)
-        with open(os.path.join(game_dir, 'scores.txt'), 'w') as f:
-            for score in score_history:
-                f.write(str(score) + '\n')
